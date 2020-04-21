a0cd1a9bc8c1856aecb9b07a312e086296031c78
Ken <a01056715@gmail.com>
2020-04-21 11:33:15 -0700

    added routes directory

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
 
+// Routes
+const mainRoute = require('./routes/mainRoute');
+
 app.use(cors('*'));
 
-app.use('/', (req, res) => res.send('testing'));
+app.use('/', mainRoute);
 
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`);
