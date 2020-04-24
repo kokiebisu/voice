3d3be9c94a48fb7d4a2b6c61be7156873b571d41
Ken <a01056715@gmail.com>
2020-04-24 12:21:56 -0700

    completed login feature on server

--- server/api/users.js
+++ server/api/users.js
@@ -6,36 +6,37 @@ firebase.initializeApp(config);
 
 const { validateLoginData, validateSignUpData } = require('../util/validators');
 
-const login = (req, res) => {
+// Login for Teacher
+const login = async (req, res) => {
   const user = {
     email: req.body.email,
     password: req.body.password,
   };
 
-  console.log('user', user);
+  const { valid, errors } = validateLoginData(user);
+  if (!valid) return res.status(400).json(errors);
 
-  // const { valid, errors } = validateLoginData(user);
-  // if (!valid) return res.status(400).json(errors);
-
-  // try {
-  //   firebase
-  //     .auth()
-  //     .signInWithEmailAndPassword(user.email, user.password)
-  //     .then((data) => {
-  //       return data.user.getIdToken();
-  //     })
-  //     .then((token) => {
-  //       return response.json({ token });
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //       return response
-  //         .status(403)
-  //         .json({ general: 'wrong credentials, please try again' });
-  //     });
-  // } catch (err) {
-  //   throw new Error(err);
-  // }
+  try {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(user.email, user.password)
+      .then((data) => {
+        const token = data.user.getIdToken();
+        return token;
+      })
+      .then((token) => {
+        const jsonToken = res.json({ token });
+        return jsonToken;
+      })
+      .catch((error) => {
+        console.error(error);
+        return response
+          .status(403)
+          .json({ general: 'wrong credentials, please try again' });
+      });
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 module.exports = { login };
