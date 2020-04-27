e8adf553d9096ccd599afce6eea6ff484aadd040
David Li <davidzhenhe@gmail.com>
2020-04-27 15:13:18 -0700

    added the styling for student authentication screen

--- client/src/screens/StudentAuthenticationScreen.js
+++ client/src/screens/StudentAuthenticationScreen.js
@@ -6,9 +6,59 @@
  * Dependencies
  */
 import React, { useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, ImageBackground, Image, } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+const styles = StyleSheet.create({
+  textbox: {
+    position: 'absolute', 
+    width: 293,
+    height: 73,
+    left: 61,
+    top: 410,
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: 'grey',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,  
+    elevation: 5
+  },
+
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  logo:{
+    position: 'absolute', 
+    top: 159,
+  },
+
+  buttonContainer: {  
+    position: 'absolute', 
+    width: 293,
+    height: 73,
+    left: 61,
+    top: 559,
+    backgroundColor: '#03a5fc',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#000000',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,  
+    elevation: 5 ,
+    alignItems: 'center',
+    justifyContent: 'center',
+  
+  },  
+
+ });
 export default () => {
   /**
    * Used to navigate between the screens
@@ -22,19 +72,24 @@ export default () => {
 
   return (
     <>
-      <View>
-        <TextInput
+      <ImageBackground source={require('../img/images.jpg')} style={styles.container}>
+        <Image source ={require('../img/Voice.png')} style={styles.logo}></Image>
+        <TextInput style={styles.textbox}
           placeholder='Enter the session id'
           value={session}
           onChangeText={(text) => setSession(text)}
         />
-        <Button
-          title='Join Session'
+        <View style={styles.buttonContainer}>  
+        <Button 
+          title='Enter' 
+          color="black"  
           onPress={() =>
             navigation.navigate('Student Session', { sessionId: session })
           }
         />
-      </View>
+        </View>
+
+      </ImageBackground>
     </>
   );
 };
