39c55ec93781e33db35b6946db0ef2e06f7607bc
David Li <zli173@my.bcit.ca>
2020-04-30 15:46:58 -0700

    styling the createsession screen

--- client/src/screens/CreateSessionScreen.js
+++ client/src/screens/CreateSessionScreen.js
@@ -6,9 +6,56 @@
  * Dependencies
  */
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, ImageBackground, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+const styles = StyleSheet.create({ 
+  textbox: {
+    position: 'absolute', 
+    width: 293,
+    height: 73,
+    left: 51,
+    top: 350,
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: 'grey',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,  
+    elevation: 5
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    position: 'absolute', 
+    top: 109,
+  },
+
+  buttonContainer: {  
+    position: 'absolute', 
+    width: 293,
+    height: 73,
+    left: 51,
+    top: 479,
+    backgroundColor: '#03a5fc',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#000000',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,  
+    elevation: 5 ,
+    alignItems: 'center',
+    justifyContent: 'center',
+  
+  },  
+});
 export default () => {
   /**
    * Used to navigate between the screens
@@ -22,18 +69,20 @@ export default () => {
 
   return (
     <>
-      <View>
-        <Text>Course</Text>
-        <TextInput
+     <ImageBackground source={require('../img/images.jpg')} style={styles.container}>
+     <Image source ={require('../img/Voice.png')} style={styles.logo}></Image>
+        <TextInput style={styles.textbox}
           placeholder='Enter the Course Name'
           value={course}
           onChangeText={(text) => setCourse(text)}
         />
-        <Button
-          title='Create Session'
+        <TouchableOpacity
+          style={styles.buttonContainer}
           onPress={() => navigation.navigate('Teacher Session', { course })}
-        />
-      </View>
+        >
+          <Text>Create Session</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </>
   );
 };
