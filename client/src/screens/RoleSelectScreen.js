da28a850f2481ae71b53e224f80af35fa91c18c2
David Li <zli173@my.bcit.ca>
2020-04-29 17:53:17 -0700

    add new img, styling role select screen and restyling the studentauthentication screen

--- client/src/screens/RoleSelectScreen.js
+++ client/src/screens/RoleSelectScreen.js
@@ -4,9 +4,65 @@
 
 // Dependencies
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, StyleSheet, ImageBackground, Image,} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+const styles = StyleSheet.create({ 
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    position: 'absolute', 
+    top: 109,
+  },
+  role:{
+    position: 'absolute', 
+    top: 249,
+  },
+  buttonContainer1: {  
+    position: 'absolute', 
+    width: 293,
+    height: 73,
+    left: 51,
+    top: 479,
+    backgroundColor: '#03a5fc',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#000000',
+    borderRadius: 50,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,  
+    elevation: 5 ,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  
+  },  
+  buttonContainer2: {  
+    position: 'absolute', 
+    width: 293,
+    height: 73,
+    left: 51,
+    top: 350,
+    backgroundColor: '#03a5fc',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#000000',
+    borderRadius: 50,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,  
+    elevation: 5 ,
+    alignItems: 'center',
+    justifyContent: 'center',
+  
+  },  
+ });
 export default () => {
   /**
    * Used to navigate between screens
@@ -15,16 +71,22 @@ export default () => {
 
   return (
     <>
-      <View>
+      <ImageBackground source={require('../img/images.jpg')} style={styles.container}>
+      <Image source ={require('../img/Voice.png')} style={styles.logo}></Image>
+      <Image source ={require('../img/role.png')} style={styles.role}></Image>
+        <View style={styles.buttonContainer1}>  
         <Button
           title='Student'
           onPress={() => navigation.navigate('Student Authentication')}
         />
+        </View>
+        <View style={styles.buttonContainer2}>  
         <Button
           title='Teacher'
           onPress={() => navigation.navigate('Teacher Create Session')}
         />
-      </View>
+        </View>
+      </ImageBackground>
     </>
   );
 };
--- client/src/screens/StudentAuthenticationScreen.js
+++ client/src/screens/StudentAuthenticationScreen.js
@@ -14,8 +14,8 @@ const styles = StyleSheet.create({
     position: 'absolute', 
     width: 293,
     height: 73,
-    left: 61,
-    top: 410,
+    left: 51,
+    top: 350,
     backgroundColor: '#FFFFFF',
     borderWidth: 1,
     borderStyle: 'solid',
@@ -35,15 +35,15 @@ const styles = StyleSheet.create({
 
   logo:{
     position: 'absolute', 
-    top: 159,
+    top: 109,
   },
 
   buttonContainer: {  
     position: 'absolute', 
     width: 293,
     height: 73,
-    left: 61,
-    top: 559,
+    left: 51,
+    top: 479,
     backgroundColor: '#03a5fc',
     borderWidth: 1,
     borderStyle: 'solid',
