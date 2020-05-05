fdb0915d081aa2c9217198a32c82cc989a0de5e9
Berty <BertNocon2@gmail.com>
2020-05-05 12:25:18 -0700

    styling teacher side "Create Course"

--- client/src/screens/CreateSessionScreen.js
+++ client/src/screens/CreateSessionScreen.js
@@ -6,9 +6,29 @@
  * Dependencies
  */
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import {View, Text, TextInput, Button, StyleSheet, ImageBackground, Image} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+const styles = StyleSheet.create({
+  textinput: {
+    position: 'absolute',
+    width: 293,
+    height: 300,
+    left: -50,
+    top: -200,
+    fontSize: 30
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  logo: {
+    position: 'absolute',
+    top: 109,
+  },
+});
 export default () => {
   /**
    * Used to navigate between the screens
@@ -22,9 +42,12 @@ export default () => {
 
   return (
     <>
+    <ImageBackground
+        source={require('../img/images.jpg')}
+        style={styles.container}>
+      <Image source={require('../img/Voice.png')} style={styles.logo}></Image>
       <View>
-        <Text>Course</Text>
-        <TextInput
+        <TextInput style={styles.textinput}
           placeholder='Enter the Course Name'
           value={course}
           onChangeText={(text) => setCourse(text)}
@@ -34,6 +57,7 @@ export default () => {
           onPress={() => navigation.navigate('Teacher Session', { course })}
         />
       </View>
+    </ImageBackground>
     </>
   );
 };
