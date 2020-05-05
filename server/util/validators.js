a274d6267653fd4bf5d5769d37222862c05285eb
craiggoodison <cgoodison1@my.bcit.ca>
2020-05-05 10:43:43 -0700

    added authenication to validators.js

--- server/util/validators.js
+++ server/util/validators.js
@@ -6,16 +6,61 @@
  * Validates whether if the given string is empty
  * @param string
  */
+
+import React, { useState } from 'react';
+
 const isEmpty = (string) => {
   if (string.trim() === '') return true;
   else return false;
 };
+
+// const __doCreateUser = async (email, password) => {
+//   try {
+//     let response = await auth().createUserWithEmailAndPassword(
+//         email,
+//         password
+//     )
+//     if (response && response.user) {
+//       Alert.alert("Success ✅", "Account created successfully")
+//     }
+//   } catch (e) {
+//     console.error(e.message)
+//   }
+// }
+//
+// const validateSignIn = () => {
+//   const [email, setEmail] = useState("")
+//   const [password, setPassword] = useState("")
+//   const [fetching, setFetching] = useState(false)
+//   const [error, setError] = useState("")
+//   const [isValid, setValid] = useState(true)
+//   const pattern = {[ "^.{8,}$" + "?=.*\\d" + "?=.*[A-Z]"]}
+//
+//   const __doSignUp = () => {
+//     if (!email) {
+//       setError("Email required *")
+//       setValid(false)
+//       return
+//     } else if (!password && password.trim() && password.length > 9) {
+//       setError("Weak password, minimum 8 chars")
+//       setValid(false)
+//       return
+//     } else if (!__isValidEmail(email)) {
+//       setError("Invalid Email")
+//       setValid(false)
+//       return
+//     }
+//
+//     __doCreateUser(email, password)
+//   }
+//
 
 /**
  * Validates whether if the login input is valid
  * @param data
  */
 const validateLoginData = (data) => {
+  validateSignIn()
   let errors = {};
   if (isEmpty(data.email)) errors.email = 'Must not be empty';
   if (isEmpty(data.password)) errors.password = 'Must not be  empty';
@@ -31,9 +76,12 @@ const validateLoginData = (data) => {
  */
 const isEmail = (email) => {
   const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   if (email.match(emailRegEx)) return true;
   else return false;
 };
+
+
 
 /**
  * Validates whether if the signup input is valid
