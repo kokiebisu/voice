1655139900c27926e93482a110da3e6e47ea0dce
Ken <a01056715@gmail.com>
2020-05-06 11:55:16 -0700

    fixed bug of feedback button not pressable

--- client/src/screens/TeacherSessionScreen.js
+++ client/src/screens/TeacherSessionScreen.js
@@ -37,7 +37,7 @@ export default () => {
    * Creates a room with a randomly generated session id
    */
   useEffect(() => {
-    socket = io(endpoint, { forceNode: true });
+    socket = io(endpoint);
     roomId = generateSessionId(5);
     setSessionId(roomId);
     socket.emit('createRoom', roomId);
