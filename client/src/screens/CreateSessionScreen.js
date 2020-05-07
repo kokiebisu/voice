f1c7455ec0443d3d37f3c395183609315841cdfa
Berty <BertNocon2@gmail.com>
2020-05-06 19:10:14 -0700

    styling teacher side "Create Session" added button to load previous entered courses

--- client/src/screens/CreateSessionScreen.js
+++ client/src/screens/CreateSessionScreen.js
@@ -21,22 +21,50 @@ const styles = StyleSheet.create({
     marginBottom: 200,
     marginTop: 15,
   },
+  textviewcontainer: {
+    flex: 1,
+    alignItems: 'center',
+    width: 150
+  },
   courseContentContainer: {
     alignSelf: 'flex-end',
     position: 'absolute',
     bottom: 20,
-    fontSize: 20,
-    width: 250,
+    fontSize: 30,
+    width: 350,
     height: 300,
-    paddingLeft: 20,
+    paddingLeft: 230,
+  },
+  buttonContainer1: {
+    // position: 'absolute',
+    width: 105,
+    height: 73,
+    // left: 51,
+    // top: 350,
+    marginTop: -200,
+    marginLeft: 130,
+    backgroundColor: '#03a5fc',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#000000',
+    borderRadius: 50,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   buttonContainer2: {
     // position: 'absolute',
-    width: 293,
+
+    width: 105,
     height: 73,
     // left: 51,
     // top: 350,
     marginTop: -100,
+    marginRight: 130,
     backgroundColor: '#03a5fc',
     borderWidth: 1,
     borderStyle: 'solid',
@@ -68,17 +96,27 @@ export default () => {
         source={require('../img/images.jpg')}
         style={styles.container}>
       <Image source={require('../img/Voice.png')} style={styles.logo}></Image>
-      <View>
+      <View style={styles.textviewcontainer}>
         <TextInput style={styles.courseContentContainer}
           placeholder='Course Name'
           value={course}
           onChangeText={(text) => setCourse(text)}
         />
+      </View>
+      <View style={styles.container}>
         <TouchableOpacity
             style={styles.buttonContainer2}
 
-            onPress={() => navigation.navigate('Teacher Create Session')}>
+            onPress={() => navigation.navigate('Teacher Session', { course })}>
           <Text style={styles.titleText}>Create Session</Text>
+        </TouchableOpacity>
+      </View>
+    <View style={styles.container}>
+        <TouchableOpacity
+            style={styles.buttonContainer1}
+
+            onPress={() => navigation.navigate('Teacher Create Session')}>
+          <Text style={styles.titleText}>Course List</Text>
         </TouchableOpacity>
       </View>
     </ImageBackground>
