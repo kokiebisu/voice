9337b2a936de1f37c8c5651d88b37c733d002b77
Berty <BertNocon2@gmail.com>
2020-05-06 18:56:01 -0700

    styling teacher side "Create Session"

--- client/src/screens/CreateSessionScreen.js
+++ client/src/screens/CreateSessionScreen.js
@@ -6,27 +6,49 @@
  * Dependencies
  */
 import React, { useState } from 'react';
-import {View, Text, TextInput, Button, StyleSheet, ImageBackground, Image} from 'react-native';
+import {View, Text, TextInput, Button, StyleSheet, ImageBackground, Image, TouchableOpacity} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const styles = StyleSheet.create({
-  textinput: {
-    position: 'absolute',
-    width: 293,
-    height: 300,
-    left: -50,
-    top: -200,
-    fontSize: 30
-  },
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
-
   logo: {
+    // position: 'absolute',
+    // top: 109,
+    marginBottom: 200,
+    marginTop: 15,
+  },
+  courseContentContainer: {
+    alignSelf: 'flex-end',
     position: 'absolute',
-    top: 109,
+    bottom: 20,
+    fontSize: 20,
+    width: 250,
+    height: 300,
+    paddingLeft: 20,
+  },
+  buttonContainer2: {
+    // position: 'absolute',
+    width: 293,
+    height: 73,
+    // left: 51,
+    // top: 350,
+    marginTop: -100,
+    backgroundColor: '#03a5fc',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#000000',
+    borderRadius: 50,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 export default () => {
@@ -47,15 +69,17 @@ export default () => {
         style={styles.container}>
       <Image source={require('../img/Voice.png')} style={styles.logo}></Image>
       <View>
-        <TextInput style={styles.textinput}
-          placeholder='Enter the Course Name'
+        <TextInput style={styles.courseContentContainer}
+          placeholder='Course Name'
           value={course}
           onChangeText={(text) => setCourse(text)}
         />
-        <Button
-          title='Create Session'
-          onPress={() => navigation.navigate('Teacher Session', { course })}
-        />
+        <TouchableOpacity
+            style={styles.buttonContainer2}
+
+            onPress={() => navigation.navigate('Teacher Create Session')}>
+          <Text style={styles.titleText}>Create Session</Text>
+        </TouchableOpacity>
       </View>
     </ImageBackground>
     </>
