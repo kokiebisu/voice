e794749a647ebe55d0632f38d91495b6020ce1cb
craiggoodison <cgoodison1@my.bcit.ca>
2020-05-07 17:41:43 -0700

    added authenication to validators.js

--- server/util/validators.js
+++ server/util/validators.js
@@ -7,68 +7,36 @@
  * @param string
  */
 
-import React, { useState } from 'react';
-
 const isEmpty = (string) => {
   if (string.trim() === '') return true;
   else return false;
 };
 
-// const __doCreateUser = async (email, password) => {
-//   try {
-//     let response = await auth().createUserWithEmailAndPassword(
-//         email,
-//         password
-//     )
-//     if (response && response.user) {
-//       Alert.alert("Success ✅", "Account created successfully")
-//     }
-//   } catch (e) {
-//     console.error(e.message)
-//   }
-// }
-//
-// const validateSignIn = () => {
-//   const [email, setEmail] = useState("")
-//   const [password, setPassword] = useState("")
-//   const [fetching, setFetching] = useState(false)
-//   const [error, setError] = useState("")
-//   const [isValid, setValid] = useState(true)
-//   const pattern = {[ "^.{8,}$" + "?=.*\\d" + "?=.*[A-Z]"]}
-//
-//   const __doSignUp = () => {
-//     if (!email) {
-//       setError("Email required *")
-//       setValid(false)
-//       return
-//     } else if (!password && password.trim() && password.length > 9) {
-//       setError("Weak password, minimum 8 chars")
-//       setValid(false)
-//       return
-//     } else if (!__isValidEmail(email)) {
-//       setError("Invalid Email")
-//       setValid(false)
-//       return
-//     }
-//
-//     __doCreateUser(email, password)
-//   }
-//
+/**
+ * Validates whether if the bcit school email is valid
+ * @param email
+ */
+const isSchool = (email) => {
+  // const bcitEmailRegEx = /@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // const bcitEmailRegEx1 = /bcit.ca&/
+  const schoolRegEx = new RegExp(/bcit.ca$/);
+  return (email.match(schoolRegEx));
+
+};
 
 /**
- * Validates whether if the login input is valid
- * @param data
+ * Validates whether if the password is valid
+ * @param password
  */
-const validateLoginData = (data) => {
-  validateSignIn()
-  let errors = {};
-  if (isEmpty(data.email)) errors.email = 'Must not be empty';
-  if (isEmpty(data.password)) errors.password = 'Must not be  empty';
-  return {
-    errors,
-    valid: Object.keys(errors).length === 0 ? true : false,
-  };
-};
+const isValid = (password) => {
+// const pattern = /^.{8,}$ ?=.*\\d  ?=.*[A-Z]
+const msg = "Weak password, minimum 8 chars"
+const pattern = /^(?=.{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$/
+// if (!password || ( password.trim() && !pattern && password.length < 8))
+return (password.match(pattern));
+// if (pattern) { return" Password proper length and pattern"}
+// else { return msg }
+}
 
 /**
  * Validates whether if the email is valid
@@ -76,12 +44,26 @@ const validateLoginData = (data) => {
  */
 const isEmail = (email) => {
   const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // const emailRegEx = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
-  if (email.match(emailRegEx)) return true;
-  else return false;
+  return (email.match(emailRegEx));
+
 };
+/**
+ * Validates whether if the login input is valid
+ * @param data
+ */
 
+const validateLoginData = (data) => {
 
+  let errors = {};
+  if (isEmpty(data.email)) errors.email = 'Please an email';
+  if (isEmpty(data.password)) errors.password = 'Please fill in a password';
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
 
 /**
  * Validates whether if the signup input is valid
@@ -89,17 +71,20 @@ const isEmail = (email) => {
  */
 const validateSignUpData = (data) => {
   let errors = {};
-
   if (isEmpty(data.email)) {
-    errors.email = 'Must not be empty';
-  } else if (!isEmail(data.email)) {
-    errors.email = 'Must be valid email address';
+    errors.email = 'Please enter your school email';
+  } else if (!isEmail(data.email) && !isSchool(data.email)) {
+    errors.email = 'Must be valid school email address';
   }
+  else errors.email = "Valid email"
 
-  if (isEmpty(data.email)) errors.email = 'Must not be empty';
-  if (isEmpty(data.password)) errors.password = 'Must not be empty';
+  if (isEmpty(data.password)) errors.password = 'Please enter an email';
+  if (isValid(data.password))
+    errors.confirmPassword = 'Please confirm your password';
   if (isEmpty(data.confirmPassword))
-    errors.confirmPassword = 'Must not be empty';
+    errors.confirmPassword = 'Please confirm your password';
+  if (isValid(data.confirmPassword))
+    errors.confirmPassword = 'email';
   if (data.password !== data.confirmPassword)
     errors.confirmPassword = 'Passwords must be the same';
 
@@ -109,7 +94,15 @@ const validateSignUpData = (data) => {
   };
 };
 
+console.log(isEmail("YTVHJJK123@ll.jj.ca"))
+console.log(isEmail("YTVdddJJK123@lljj..ca"))
+console.log(isValid('Paow(23dH'))
+console.log(isValid('P1s'))
+console.log(isSchool("@bcit.ca"))
+console.log(isSchool("@bcct.ca"))
+
 module.exports = {
   validateLoginData,
   validateSignUpData,
 };
+
