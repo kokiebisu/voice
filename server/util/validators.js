b0b0bd97da3a831e27f1fec6af9b04500307b979
Ken <a01056715@gmail.com>
2020-05-11 13:45:04 -0700

    removed unnecessary logs from server

--- server/util/validators.js
+++ server/util/validators.js
@@ -20,8 +20,7 @@ const isSchool = (email) => {
   // const bcitEmailRegEx = /@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   // const bcitEmailRegEx1 = /bcit.ca&/
   const schoolRegEx = new RegExp(/bcit.ca$/);
-  return (email.match(schoolRegEx));
-
+  return email.match(schoolRegEx);
 };
 
 /**
@@ -29,14 +28,14 @@ const isSchool = (email) => {
  * @param password
  */
 const isValid = (password) => {
-// const pattern = /^.{8,}$ ?=.*\\d  ?=.*[A-Z]
-const msg = "Weak password, minimum 8 chars"
-const pattern = /^(?=.{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$/
-// if (!password || ( password.trim() && !pattern && password.length < 8))
-return (password.match(pattern));
-// if (pattern) { return" Password proper length and pattern"}
-// else { return msg }
-}
+  // const pattern = /^.{8,}$ ?=.*\\d  ?=.*[A-Z]
+  const msg = 'Weak password, minimum 8 chars';
+  const pattern = /^(?=.{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$/;
+  // if (!password || ( password.trim() && !pattern && password.length < 8))
+  return password.match(pattern);
+  // if (pattern) { return" Password proper length and pattern"}
+  // else { return msg }
+};
 
 /**
  * Validates whether if the email is valid
@@ -46,8 +45,7 @@ const isEmail = (email) => {
   const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   // const emailRegEx = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
-  return (email.match(emailRegEx));
-
+  return email.match(emailRegEx);
 };
 /**
  * Validates whether if the login input is valid
@@ -55,7 +53,6 @@ const isEmail = (email) => {
  */
 
 const validateLoginData = (data) => {
-
   let errors = {};
   if (isEmpty(data.email)) errors.email = 'Please an email';
   if (isEmpty(data.password)) errors.password = 'Please fill in a password';
@@ -75,18 +72,16 @@ const validateSignUpData = (data) => {
     errors.email = 'Please enter your school email';
   } else if (!isEmail(data.email) && !isSchool(data.email)) {
     errors.email = 'Must be valid school email address';
-  }
-  else errors.email = "Valid email"
+  } else errors.email = 'Valid email';
 
   if (isEmpty(data.password)) errors.password = 'Please enter an email';
   if (isValid(data.password))
     errors.confirmPassword = 'Please confirm your password';
   if (isEmpty(data.confirmPassword))
-    errors.confirmPassword = "Confirm your password";
+    errors.confirmPassword = 'Confirm your password';
   if (data.password !== data.confirmPassword)
     errors.confirmPassword = 'Passwords must be the same';
-  if (isValid(data.confirmPassword))
-    errors.confirmPassword = 'OK';
+  if (isValid(data.confirmPassword)) errors.confirmPassword = 'OK';
 
   return {
     errors,
@@ -94,15 +89,7 @@ const validateSignUpData = (data) => {
   };
 };
 
-console.log(isEmail("YTVHJJK123@ll.jj.ca"))
-console.log(isEmail("YTVdddJJK123@lljj..ca"))
-console.log(isValid('Paow(23dH'))
-console.log(isValid('P1s'))
-console.log(isSchool("@bcit.ca"))
-console.log(isSchool("@bcct.ca"))
-
 module.exports = {
   validateLoginData,
   validateSignUpData,
 };
-
