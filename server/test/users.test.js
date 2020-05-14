96c3529a492cac622f43f57959cefe1d1422874b
craiggoodison <cgoodison1@my.bcit.ca>
2020-05-14 13:43:05 -0700

    added user.test.js file

--- server/test/users.test.js
+++ server/test/users.test.js
@@ -1,17 +1,24 @@
-// const signup = require('../api/users.js');
-// const expect = require(expect);
-// const assert  = require('assert');
-// const chai = require('chai')
-// const chaiAsPromised = require('chai-as-promised');
-// // const expect = chai.expect
-// chai.use(chaiAsPromised)
+const signup = require('../api/users.js');
+const expect = require(expect);
+const assert  = require('assert');
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised');
+// const expect = chai.expect
+chai.use(chaiAsPromised)
 
 
-// describe('signup', () => {
-//     it('should catch an error', async () => {
-//         return expect(signup(async (req, res))).to.be.rejected
-//     })
-// })
+describe('signup()', () => {
+    it('should catch an error', async () => {
+        return expect(signup(async (req, res))).to.be.rejected
+    })
+})
+
+describe('signup()', () => {
+    it('2 + 2 is 4', async () => {
+        const p = await signup(2, 2)
+        expect(p).to.equal(4);
+    })
+})
 
 // describe ('../api/users.js', () => {
 //     describe('signup', () => {
