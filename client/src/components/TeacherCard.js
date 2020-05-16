5b0319e89b97ece4c78ce3d9e3b9c6078487ff69
David Li <zli173@my.bcit.ca>
2020-05-15 18:43:40 -0700

    styled the teacher card based on figma

--- client/src/components/TeacherCard.js
+++ client/src/components/TeacherCard.js
@@ -13,6 +13,7 @@ export const TeacherCard = ({ respond, feedbackName, length, imgSrc,}) => {
         <Text style={styles.text}>
           {feedbackName}: {length}
         </Text>
+        <View style={{backgroundColor:'#FF5C5C', height: 125, width:25,borderTopRightRadius:10, borderBottomRightRadius:10,marginLeft:146,marginTop:-117}}></View>
       </TouchableOpacity>
       </View>
     </>
@@ -27,7 +28,7 @@ const styles = StyleSheet.create({
     width: 256,
     height: 125,
     borderRadius: 10,
-    backgroundColor: '#FF5C5C',
+    backgroundColor: '#FFFFFF',
     marginBottom: 20
   },
   text:{
