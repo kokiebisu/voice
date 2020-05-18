d486d4c7baec0a7d598ade641fa623ad9f7bf049
David Li <zli173@my.bcit.ca>
2020-05-18 14:38:06 -0700

    styled the voice section

--- client/src/components/Voice.js
+++ client/src/components/Voice.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity, Text, Image, StyleSheet} from 'react-native';
 
 export const Voice = ({
   feedbackName,
@@ -11,8 +11,9 @@ export const Voice = ({
 }) => {
   return (
     <>
-      <View>
-        <Text>{feedbackName}</Text>
+      <View style={styles.card}>
+    
+        <Text style={styles.text}>{feedbackName}</Text>
         {isPressed ? null : (
           <TouchableOpacity
             onPress={() => {
@@ -20,10 +21,46 @@ export const Voice = ({
               disableFeedbacks();
               disableIAgree();
             }}>
-            <Text>I agree</Text>
+              <View style={styles.button2}>
+            <Text style={styles.button1}>I agree</Text>
+            </View>
           </TouchableOpacity>
         )}
       </View>
     </>
   );
 };
+const styles = StyleSheet.create({
+  card:{
+    width: 100,
+    height: 120,
+    borderRadius: 12,
+    backgroundColor: '#C4C4C4',
+    marginTop: 40,
+    marginLeft:20
+    
+  },
+  text:{
+    fontSize:15, 
+    fontWeight: 'bold',
+    paddingTop: 50,
+    paddingLeft: 15,
+   
+    
+  },
+  button1:{
+      paddingLeft:13,
+      color: 'white',
+  },
+  button2:{
+    width: 72,
+    height:21,
+    backgroundColor:'#00263B',
+    borderRadius: 10,
+    marginTop: 20,
+    marginLeft: 15
+    
+}
+
+
+});
