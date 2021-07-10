ccd511b344fb16c0b41ad5f656a3350bad277af5
kokiebisu <a01056715@gmail.com>
2021-07-10 13:36:44 -0700

    fix: remove cred

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -3,15 +3,15 @@
  */
 
 const config = {
-  apiKey: 'AIzaSyA9acS46FyiKLPHYssUZ536BbxxNdf09pQ',
-  authDomain: 'voice-3cf58.firebaseapp.com',
-  databaseURL: 'https://voice-3cf58.firebaseio.com',
-  projectId: 'voice-3cf58',
-  storageBucket: 'voice-3cf58.appspot.com',
-  messagingSenderId: '1034051388014',
-  appId: '1:1034051388014:web:dea93ae145e0256bed5c7f',
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
-const firestore_location = 'us-west3';
+const firestore_location = "us-west3";
 
 module.exports = { config, firestore_location };
